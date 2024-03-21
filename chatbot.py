992d3bd1ca5db7f256988bd6f51ecf60bcd25b3f
MBA 2017 <PongLin18@icloud.com>
2024-03-21 17:30:23 +0800

    Fixing chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 ##chatbot.py
 from telegram import Update
-from telegram.ext import (Updater,CommandHandler, MessageHandler, Filters, CallbackContext)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
 
 import configparser
 import logging
@@ -20,14 +20,14 @@ def main():
 
 	#you can set this logging module, so you will know when
 	#and why things do not as expected Meanwhile, update your config.ini as:
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-			 level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 	#register a dispatcher to handle message: here we register an echo dispatchr
     echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
     dispatcher.add_handler(echo_handler)
 	#on different commands - answer in Telegram
 	dispathcer.add_handler(CommandHandler("add",add))
 	dispathcer.add_handler(CommandHandler("help", help_command))
+
 	#To start the bot:
     updater.start_polling()
     updater.idle()
@@ -39,6 +39,7 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 	#Define a few command handlers. These usually take the two arguments update and
 	#context. Error handlers also receive the raised TelegramError object in error.
+
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Helping you helping you.')
@@ -49,6 +50,7 @@ def add(update: Update, context: CallbackContext) -> None:
         logging.info(context.args[0])
         msg = context.args[0]   # /add keyword <-- this should store the keyword
         redis1.incr(msg)
+
         update.message.reply_text('You have said ' + msg +  ' for ' +
                         redis1.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
